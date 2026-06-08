30df40a496f8307b9297803cbe3d2ade036e7f6a
Tuyền Nguyễn <thuylucbonjajz@gmail.com>
2026-06-08 13:20:08 +0700

    test: test case code

---
+++ src/styles/index.ts
@@ -0,0 +1,7 @@
+// File	Mục đích	Nội dung chính	Phạm vi ảnh hưởng
+// variables.scss	Lưu trữ giá trị tái sử dụng	Màu sắc, spacing, font sizes, breakpoints	Toàn dự án (import vào file khác)
+// mixins.scss	Lưu trữ đoạn code tái sử dụng	Media queries, flex center, grid layouts	Toàn dự án (import vào file khác)
+// global.scss	Style áp dụng toàn cục	Reset CSS, body styles, class container	Toàn bộ trang web
+export * from "./variables.scss";
+export * from "./mixins.scss";
+export * from "./global.scss";
